40ed4a0a9437807622a990ec1ed1fcf89e79ebf8
TANMAY <tanmay@TANMAYs-MacBook-Air.local>
2026-01-17 17:12:31 +0530

    fix: search now scans actual file system instead of only open tabs

---
+++ next.config 2.js
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { dev, isServer }) => {
+    if (dev) {
+      config.devServer = {
+        ...config.devServer,
+        headers: {
+          'Content-Type': 'application/javascript; charset=utf-8',
+        },
+      }
+    }
+    
+    // Exclude database drivers from bundle (not needed on Vercel)
+    if (isServer) {
+      config.externals = config.externals || []
+      config.externals.push({
+        'mysql2/promise': 'commonjs mysql2/promise',
+        'pg': 'commonjs pg',
+        'sqlite3': 'commonjs sqlite3',
+        'sqlite': 'commonjs sqlite',
+        'mongodb': 'commonjs mongodb'
+      })
+    }
+    
+    return config
+  },
+}
+
+module.exports = nextConfig
